969a480101df8d4234d6d02ae4d92f552b1bc37d
lilei <1378692567@qq.com>
2020-03-23 22:14:12 +0800

    重复尝试添加retry

--- src/com/tb/function/StreamDemo.java
+++ src/com/tb/function/StreamDemo.java
@@ -6,6 +6,9 @@ import java.nio.file.Paths;
 import java.util.Currency;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 /**
@@ -16,6 +19,37 @@ public class StreamDemo {
 
     public static void main(String[] args) {
 
+        // testRetry();
+        String ss = retry(5, 100, x -> x * Math.random() > 50 ? "sucess" : null);
+        System.out.println(ss);
+
+    }
+
+    //
+    public static void testRetry() {
+        String ok = Stream.generate(() -> {
+            System.out.println("test");
+            return Math.random() > 0.5 ? "ok" : null;
+        })
+                .limit(5)
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElse(null);
+        System.out.println(ok);
+
+
+    }
+
+    public static <T, V> V retry(int i, T t, Function<T, V> function) {
+        return Stream.generate(() -> function.apply(t))
+                .limit(i)
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElse(null);
+    }
+
+
+    public void test() {
         // 流中间操作
         Stream<String> stream = Stream.of("benny", "taina", "purnima")
                 .map(x -> {
@@ -39,8 +73,6 @@ public class StreamDemo {
         Map<Currency, Double> exchangeToEur = Map.of(Currency.getInstance(Locale.US), 0.96, Currency.getInstance(Locale.CANADA),
                 1.56, Currency.getInstance(Locale.ENGLISH), 2.1);
         System.out.println(exchangeToEur);
-
-
     }
 
 
